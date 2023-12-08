e5ec1ee472876dc29c7de473c107b38468bbf831
andrw <2024240@creightonprep.org>
2023-12-08 17:19:16 -0600

    things auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueCenter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueCenter.java
@@ -52,20 +52,32 @@ public class AutoBlueCenter extends LinearOpMode {
         ML.turn(pi/2);
         //insert camera code HERE
         if(cameraOutcome == 3)
-            ML.turn(pi/2);
+        {
+            ML.turn(pi / 2);
+        }
         if(cameraOutcome == 2 || cameraOutcome == 3)
+        {
             ML.Intake(take);
+        }
         if(cameraOutcome == 1 || 2)
+        {
             ML.turn(pi/2);
+        }
         ML.move(-1*a,0);
         if(cameraOutcome == 1)
+        {
             ML.Intake(take);
+        }
         //The following segment is to be INCLUDED if we start far from the backdrop, but EXCLUDED if we start near it
         ML.forward(-2*a);
         if(cameraOutcome == 1)
+        {
             ML.move(b,1);
+        }
         if(cameraOutcome == 3)
+        {
             ML.move(-b,1);
+        }
         //code for extending arm/opening claw here
         //if this is the far code, do nothing. if it is the near code, either parkcenter and do nothing or parkout and move to the edge of the field.
     }
